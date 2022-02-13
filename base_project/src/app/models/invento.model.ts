3dc67e41e0495b5df92a1603f23fef408c8d0a18
marioEzquerro <marioezquerroruiz@gmail.com>
2022-02-13 16:12:05 +0100

    Proyecto finalizado

--- base_project/src/app/models/invento.model.ts
+++ base_project/src/app/models/invento.model.ts
@@ -2,12 +2,14 @@ export class Invento {
   id: number;
   nombre: string | null;
   desc: string | null;
+  longDesc: string | null;
   img: string | null;
 
   constructor() {
     this.id = 0;
     this.nombre = null;
     this.desc = null;
+    this.longDesc = null;
     this.img = null;
   }
 }
